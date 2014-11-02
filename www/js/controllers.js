98623d541f3b39f2fe3e5d895658472079734a00
Sol Tran <sol@shiphawk.com>
2014-11-01 22:55:10 -0700

    Send user to account page on successful signup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('RegistrationCtrl', function($scope, AuthFactory) {
+.controller('RegistrationCtrl', function($scope, AuthFactory, $state) {
   $scope.signupForm = {};
   console.log("hi");
   console.log($scope.signupForm);
@@ -14,10 +14,12 @@ angular.module('starter.controllers', [])
     console.log($scope.signupForm)
 
     AuthFactory.register($scope.signupForm)
-      .then(function(){
-        console.log('Yay successfully registered ', $scope.signupForm.name)
+      .then(function(data){
+        console.log('Yay successfully registered ', data)
+        $state.go('tab.account')
 
       }, function(){
+
         console.log('Did not work :/')
       })
 
